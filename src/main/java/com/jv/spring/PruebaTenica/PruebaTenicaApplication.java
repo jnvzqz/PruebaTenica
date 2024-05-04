49b8f2573d02f8a2d3d15d680503c7c6dcc5a598
jnvzqz <jonevazquezsolano@gmail.com>
2024-05-04 12:48:56 +0200

    initial commit and project setup

---
+++ src/main/java/com/jv/spring/PruebaTenica/PruebaTenicaApplication.java
@@ -0,0 +1,13 @@
+package com.jv.spring.PruebaTenica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTenicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaTenicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jv/spring/PruebaTenica/PruebaTenicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jv.spring.PruebaTenica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaTenicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
